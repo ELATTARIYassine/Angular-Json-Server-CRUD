60bd05cf4e080e82addc25160eaddb81e3a4f75b
El attari Yassine <yassineattari64@gmail.com>
2020-01-25 21:16:14 +0100

    [Coding] Added service & cleanning test files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListUserComponent } from './components/list-user/list-user.component';
 import { UserFormComponent } from './components/user-form/user-form.component';
+import { UserService } from './_services/user.service';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,7 @@ import { UserFormComponent } from './components/user-form/user-form.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [UserService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
