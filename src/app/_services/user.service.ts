68ac17a198a3400314a50e9dafa35ae9d30230b1
El attari Yassine <yassineattari64@gmail.com>
2020-01-25 21:25:56 +0100

    [Coding] code user service

--- src/app/_services/user.service.ts
+++ src/app/_services/user.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { User } from '../Model/user';
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  private baseUrl :string = "http://localhost:3000/users";
+  constructor(private http: HttpClient) { }
+
+  findAll(){
+    return this.http.get<User[]>(this.baseUrl);
+  }
+  deleteUser(id: number){
+    return this.http.delete<User>(this.baseUrl+`/${id}`);
+  }
+  createUser(user: User){
+    return this.http.post<User>(this.baseUrl, user);
+  }
+  updateUser(user: User){
+    return this.http.put<User>(this.baseUrl, user);
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListUserComponent } from './components/list-user/list-user.component';
@@ -15,7 +15,8 @@ import { UserService } from './_services/user.service';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [UserService],
   bootstrap: [AppComponent]
